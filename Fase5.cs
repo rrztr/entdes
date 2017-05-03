aa9b90bab8c4a01e8bfcef17f8e2662df4d61310
Rafa Ruiz <rafaruizasir@gmail.com>
2017-05-03 20:06:41 +0200

    Añadida la fase 5.

---
+++ Fase5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica2_2EV
+{
+    class Fase5
+    {
+        //Aqui los barcos se inscribirán en el camino de vuelta.
+
+        public static List<Barcos> lista_vuelta_rapido = new List<Barcos>();
+        public static List<Barcos> lista_vuelta_lento = new List<Barcos>();
+
+        //Cuando el barco llega al fin de la vida
+
+        public void salida_via_rapida()
+        {
+            Barcos bviar = lista_vuelta_rapido[0];
+            lista_vuelta_rapido.RemoveAt(0);
+            Fase4.cola_salida.Add(bviar);
+        }
+
+        public void salida_via_lenta()
+        {
+            Barcos bvial = via_lenta[0];
+            via_lenta.RemoveAt(0);
+            Fase4.cola_salida.Add(bvial);
+        }
+
+    }
+}
