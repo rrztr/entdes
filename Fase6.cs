71a0fce4918b435ea7b8548fc6c23019161b7456
Rafa Ruiz <rafaruizasir@gmail.com>
2017-05-03 20:25:26 +0200

    Añadimos la fase 6.

---
+++ Fase6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica2_2EV
+{
+    class Fase6
+    {
+       
+
+        public static List<Barcos> lista_vuelta_rapido2 = new List<Barcos>();
+        public static List<Barcos> lista_vuelta_lento2 = new List<Barcos>();
+
+
+    }
+}
